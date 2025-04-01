be9c1d2ced08708b1d348b53c0edfca590a01f74
Sohan Ghosh <isohan2007@gmail.com>
2025-04-01 13:02:22 +0530

    add a store into the project

---
+++ src/store/Index.jsx
@@ -0,0 +1,23 @@
+import { legacy_createStore } from "redux";
+
+// function that takes the previous state and provides a new state obj
+const counterReducer = (state = { counter: 0 }, action) => {
+  if (action.type === "increment") {
+    return {
+      counter: state.counter + 1,
+    };
+  }
+
+  if (action.type === "decrement") {
+    return {
+      counter: state.counter - 1,
+    };
+  }
+
+  return state;
+};
+
+// the store requires an argument that points to the reducer function
+const store = legacy_createStore(counterReducer);
+
+export default store;
