5998d4fb97fde1c3441d5fc235774301a0791ea7
Sohan Ghosh <isohan2007@gmail.com>
2025-04-01 15:10:26 +0530

    handle multiple states

--- src/store/Index.jsx
+++ src/store/Index.jsx
@@ -1,22 +1,34 @@
 import { legacy_createStore } from "redux";
 
 // function that takes the previous state and provides a new state obj
-const counterReducer = (state = { counter: 0 }, action) => {
+const initialState = { counter: 0, showCounter: true };
+const counterReducer = (state = initialState, action) => {
   if (action.type === "increment") {
     return {
       counter: state.counter + 1,
+      showCounter: state.showCounter,
+      // need to add all the states that we are handling cuz redux won't merge the changes with the obj instead replace the obj
     };
   }
 
   if (action.type === "increase") {
     return {
       counter: state.counter + action.amount,
+      showCounter: state.showCounter,
     };
   }
 
   if (action.type === "decrement") {
     return {
       counter: state.counter - 1,
+      showCounter: state.showCounter,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      showCounter: !state.showCounter,
+      counter: state.counter,
     };
   }
 
